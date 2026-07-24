03f8a48bbcb3f12b67478f39e9b535f74108ff33
feumort <2197333163@qq.com>
2026-07-24 17:04:21 +0800

    Add app/src/main/java/com/xq/bilibilidownloader/VideoInfo.java

---
+++ app/src/main/java/com/xq/bilibilidownloader/VideoInfo.java
@@ -0,0 +1,10 @@
+package com.xq.bilibilidownloader;
+
+public class VideoInfo {
+    public String bvid;
+    public long aid;
+    public long cid;
+    public String title;
+    public String upName;
+    public String cover;
+}
