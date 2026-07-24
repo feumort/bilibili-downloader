2dd639ec792b1725c319134f8ca2c1f68971aaf4
feumort <2197333163@qq.com>
2026-07-24 18:51:08 +0800

    Add multi-part (分P) batch download support

--- app/src/main/java/com/xq/bilibilidownloader/VideoInfo.java
+++ app/src/main/java/com/xq/bilibilidownloader/VideoInfo.java
@@ -1,5 +1,8 @@
 package com.xq.bilibilidownloader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VideoInfo {
     public String bvid;
     public long aid;
@@ -7,4 +10,11 @@ public class VideoInfo {
     public String title;
     public String upName;
     public String cover;
+    public List<Page> pages = new ArrayList<>();
+
+    public static class Page {
+        public int page;
+        public long cid;
+        public String part;
+    }
 }
